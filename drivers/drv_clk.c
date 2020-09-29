e6c257762617988fc272d20423139d1a24f4b125
张竞豪 <645902989@qq.com>
2020-09-29 10:10:49 +0800

    修改时钟源为外部时钟源，添加esp8266连接wifi功能

--- drivers/drv_clk.c
+++ drivers/drv_clk.c
@@ -20,62 +20,96 @@
 void system_clock_config(int target_freq_Mhz)
 {
     RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+      RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-    /** Initializes the CPU, AHB and APB busses clocks
-    */
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
-#if defined(STM32F100xB) || defined(STM32F100xE)
-    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6;
-#endif
-#if defined(STM32F101x6) || defined(STM32F101xB) || defined(STM32F101xE) || defined(STM32F101xG)
-    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
-#endif
-#if defined(STM32F102x6) || defined(STM32F102xB)
-    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL12;
-#endif
-#if defined(STM32F103x6) || defined(STM32F103xB) || defined(STM32F103xE) || defined(STM32F103xG)
-    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
-#endif
-#if defined(STM32F105xC) || defined(STM32F107xC)
-    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
-#endif
-    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-    {
-      Error_Handler();
-    }
-    /** Initializes the CPU, AHB and APB busses clocks
-    */
-    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                                |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+      /** Initializes the CPU, AHB and APB busses clocks
+      */
+      RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+      RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+      RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+      RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+      RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+      RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+      RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+      if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+      {
+        Error_Handler();
+      }
+      /** Initializes the CPU, AHB and APB busses clocks
+      */
+      RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                                  |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+      RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+      RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+      RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+      RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-#if defined(STM32F100xB) || defined(STM32F100xE)
-    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
-#endif
-#if defined(STM32F101x6) || defined(STM32F101xB) || defined(STM32F101xE) || defined(STM32F101xG)
-    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
-#endif
-#if defined(STM32F102x6) || defined(STM32F102xB)
-    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
-#endif
-#if defined(STM32F103x6) || defined(STM32F103xB) || defined(STM32F103xE) || defined(STM32F103xG)
-    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-#endif
-#if defined(STM32F105xC) || defined(STM32F107xC)
-        if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-#endif
-    {
-      Error_Handler();
-    }
+      if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+      {
+        Error_Handler();
+      }
 }
+
+//void system_clock_config(int target_freq_Mhz)
+//{
+//    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+//    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+//
+//    /** Initializes the CPU, AHB and APB busses clocks
+//    */
+//    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+//    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+//    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+//    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+//    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
+//#if defined(STM32F100xB) || defined(STM32F100xE)
+//    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6;
+//#endif
+//#if defined(STM32F101x6) || defined(STM32F101xB) || defined(STM32F101xE) || defined(STM32F101xG)
+//    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+//#endif
+//#if defined(STM32F102x6) || defined(STM32F102xB)
+//    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL12;
+//#endif
+//#if defined(STM32F103x6) || defined(STM32F103xB) || defined(STM32F103xE) || defined(STM32F103xG)
+//    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
+//#endif
+//#if defined(STM32F105xC) || defined(STM32F107xC)
+//    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+//#endif
+//    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+//    {
+//      Error_Handler();
+//    }
+//    /** Initializes the CPU, AHB and APB busses clocks
+//    */
+//    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+//                                |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+//    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+//    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+//    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+//    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+//
+//#if defined(STM32F100xB) || defined(STM32F100xE)
+//    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+//#endif
+//#if defined(STM32F101x6) || defined(STM32F101xB) || defined(STM32F101xE) || defined(STM32F101xG)
+//    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+//#endif
+//#if defined(STM32F102x6) || defined(STM32F102xB)
+//    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+//#endif
+//#if defined(STM32F103x6) || defined(STM32F103xB) || defined(STM32F103xE) || defined(STM32F103xG)
+//    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+//#endif
+//#if defined(STM32F105xC) || defined(STM32F107xC)
+//        if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+//#endif
+//    {
+//      Error_Handler();
+//    }
+//}
+
 
 int clock_information(void)
 {
